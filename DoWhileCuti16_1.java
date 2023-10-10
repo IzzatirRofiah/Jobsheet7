7c7ba863d25232b27f713b3d2ec71b70bf1f7c8c
IzzatirRofiah <144086912+IzzatirRofiah@users.noreply.github.com>
2023-10-10 15:35:03 +0700

    Add files via upload

---
+++ DoWhileCuti16_1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class DoWhileCuti16_1 {
+    public static void main(String[] args) {
+        Scanner sc16 = new Scanner(System.in);
+
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti : ");
+        jatahCuti = sc16.nextInt();
+
+        do {
+            System.out.print("Apakah anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc16.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")) {
+                System.out.print("Jumlah hari : ");
+                jumlahHari = sc16.nextInt();
+
+                if (jumlahHari <= jatahCuti) {
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti : " + jatahCuti);
+                } else if (jatahCuti == 0){
+                    break;
+                }else {
+                    System.out.println("Jatah cuti tidak tersedia, sisa jatah cuti anda tinggal " + jatahCuti);
+                }
+            }
+        } while (jatahCuti > 0);
+    }
+}
