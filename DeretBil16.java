650127734b4495987aff2956888941e1c32ab481
IzzatirRofiah <144086912+IzzatirRofiah@users.noreply.github.com>
2023-10-12 16:30:11 +0700

    Add files via upload

---
+++ DeretBil16.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DeretBil16 {
+    public static void main(String[] args) {
+    
+        for (int i = 1; i <= 50; i++){
+            if ((i % 3) ==0 ){
+                continue;
+            }
+            System.out.print(i + " ");
+        }
+    }
+}
